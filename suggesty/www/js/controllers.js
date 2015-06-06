d5857a51e94fe26e1b1dbfafe7ab0b53405954b2
Derrick Bauer <dbauer@simventions.com>
2015-06-06 16:44:10 -0400

    remove unused default project files

--- suggesty/www/js/controllers.js
+++ suggesty/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('suggesty.controllers', [])
 .controller('SuggestionSubmitCtrl', function($scope, $state, Suggestions) {
-  $scope.types = ['Traffic', 'Litter', 'Maintenance', 'Landscaping']
+  $scope.types = ['Traffic', 'Litter', 'Maintenance', 'Landscaping', 'Other']
   $scope.suggest = function(suggestion){
     Suggestions.submit(JSON.parse(JSON.stringify(suggestion))).then(function(){
       $state.go('tab.suggestions')
@@ -20,29 +20,4 @@ angular.module('suggesty.controllers', [])
       $scope.suggestion = res.data
     })
   })
-})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
 });
